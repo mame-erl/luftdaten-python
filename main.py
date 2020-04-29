095991422be0955457c9e3a047883d2ea9a66bb1
Markus Meier <markusm@mba2013.fritz.box>
2020-04-29 16:46:43 +0200

    Cleanup things further

--- main.py
+++ main.py
@@ -3,17 +3,10 @@
 import sys
 import os
 import yaml
-
-# Import-Pfade setzen
-sys.path.append(os.path.join(sys.path[0],"sds011"))
-sys.path.append(os.path.join(sys.path[0],"bme280"))
-
 import time
 import json
 import requests
-import numpy as np
-from sds011 import SDS011
-from Adafruit_BME280 import *
+
 
 # Config
 with open("config.yml", 'r') as ymlfile:
@@ -23,38 +16,11 @@ with open("config.yml", 'r') as ymlfile:
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
-bme280 = BME280(
-    address=0x76,
-    t_mode=BME280_OSAMPLE_8,
-    p_mode=BME280_OSAMPLE_8,
-    h_mode=BME280_OSAMPLE_8,
-)
-
-# Create an instance of your bme280
-dusty = SDS011('/dev/ttyUSB0')
-
-# Now we have some details about it
-print("SDS011 initialized: device_id={} firmware={}".format(dusty.device_id,dusty.firmware))
-
-# Set dutycyle to nocycle (permanent)
-dusty.dutycycle = 0
 
 class Measurement:
     def __init__(self):
-        pm25_values = []
-        pm10_values = []
-        dusty.workstate = SDS011.WorkStates.Measuring
-        try:
-            for a in range(8):
-                values = dusty.get_values()
-                if values is not None:
-                    pm10_values.append(values[0])
-                    pm25_values.append(values[1])
-        finally:
-            dusty.workstate = SDS011.WorkStates.Sleeping
-
-        self.pm25_value  = np.mean(pm25_values)
-        self.pm10_value  = np.mean(pm10_values)
+        self.pm25_value  = pm25_values
+        self.pm10_value  = pm10_values
         self.temperature = bme280.read_temperature()
         self.humidity    = bme280.read_humidity()
         self.pressure    = bme280.read_pressure()
@@ -64,13 +30,7 @@ class Measurement:
         if not config['luftdaten']['enabled']:
             return
 
-        self.__pushLuftdaten('https://api-rrd.madavi.de/data.php', 0, {
-            "SDS_P1":             self.pm10_value,
-            "SDS_P2":             self.pm25_value,
-            "BME280_temperature": self.temperature,
-            "BME280_pressure":    self.pressure,
-            "BME280_humidity":    self.humidity,
-        })
+
         self.__pushLuftdaten('https://api.luftdaten.info/v1/push-sensor-data/', 1, {
             "P1": self.pm10_value,
             "P2": self.pm25_value,
@@ -94,13 +54,6 @@ class Measurement:
             }
         )
 
-# extracts serial from cpuinfo
-def getSerial():
-    with open('/proc/cpuinfo','r') as f:
-        for line in f:
-            if line[0:6]=='Serial':
-                return(line[10:26])
-    raise Exception('CPU serial not found')
 
 def run():
     m = Measurement()
@@ -115,7 +68,7 @@ def run():
     m.sendInflux()
 
 
-sensorID  = config['luftdaten'].get('sensor') or ("raspi-" + getSerial())
+sensorID  = config['luftdaten'].get('sensor')
 starttime = time.time()
 
 while True:
